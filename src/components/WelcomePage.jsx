98993ebdb5a6841a4a315006f4e2ccf726ecf4b3
Karen Harutyunyan <karen@MacBook-Pro-Karen.local>
2023-12-01 20:47:40 +0300

    change text

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -3,16 +3,20 @@ import '../assets/styles/WelcomePage.scss';
 
 const WelcomePage = ({ welcomePageRef }) => {
 
+    const [avatar, setAvatar] = useState()
+
     return (
         <div className="WelcomePage" id='welcomePage' ref={welcomePageRef}>
             <div className="left">
                 <h1>FRONT-END ВЕБ-РАЗРАБОТЧИК</h1>
                 <h2>Карен Арутюнян </h2>
-                <p>Привет, меня зовут Карен. Я опытный фронтенд-разработчик с 1-2 годами интенсивной практики в создании пользовательских интерфейсов с использованием современных технологий.</p>
+                <p>Привет, меня зовут Карен. Я опытный фронтенд-разработчик, практикующий в создании пользовательских интерфейсов с использованием современных технологий.</p>
             </div>
             <div className="right">
                 <div className="avatar">
-                    <img src="" alt="" />
+                   {
+                    avatar ?  <img src="" alt="" /> : <p>Здесь должно было быть моя фотография, но что то пошло не так {"))"}.</p>
+                   }
                 </div>
             </div>
         </div>
